e3d967f5dd0ed8a148b97a63292082cc7dd7cc05
KanhaKanhaiya <thebig6project@gmail.com>
2022-06-02 14:25:38 +0530

    Better directory structure

---
+++ app/src/main/java/ml/test7777/big6/appstore/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package ml.test7777.big6.appstore.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ml.test7777.big6.appstore.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
